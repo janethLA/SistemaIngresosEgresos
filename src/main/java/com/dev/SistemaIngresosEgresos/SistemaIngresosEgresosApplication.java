d333a8708df9b1bbd28d1836b45ba49313f50b84
janeth <jl715643@gmail.com>
2021-12-02 12:04:39 -0400

    Inicio

---
+++ src/main/java/com/dev/SistemaIngresosEgresos/SistemaIngresosEgresosApplication.java
@@ -0,0 +1,13 @@
+package com.dev.SistemaIngresosEgresos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaIngresosEgresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaIngresosEgresosApplication.class, args);
+	}
+
+}
