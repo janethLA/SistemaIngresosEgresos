3b278b05715a4301655fd4b02a249ad78ce96253
janeth <jl715643@gmail.com>
2021-12-12 16:16:05 -0400

    cambios

---
+++ src/main/java/com/dev/SistemaIngresosEgresos/input/UserDataInput.java
@@ -0,0 +1,28 @@
+package com.dev.SistemaIngresosEgresos.input;
+
+public class UserDataInput {
+
+	private String name;
+	private String username;
+	private int telephone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+}
